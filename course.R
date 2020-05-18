1f73d16e088c8bf601e2f49130900373aa3f3686
Bertrand Debrosise <Bertrand_Debroise@Yahoo.fr>
2020-05-18 21:26:00 +0200

    work in progress

--- course.R
+++ course.R
@@ -1,4 +1,4 @@
-
+act1$day  
 unzip("activity.zip")
 
 library(readr)
@@ -61,8 +61,82 @@ points(835,206.170,pch=20,col="red")
 
 
 # troisieme
+# https://stackoverflow.com/questions/50919161/how-to-simply-count-number-of-rows-with-nas-r/50919195
+
+head(rowSums(is.na(activity)))
+rowSums(is.na(activity))
+        
+
+head(colSums(is.na(activity)))
+
+sum(is.na(activity))
+
+x0 <- is.na(activity$date)
+x0
+
+#�act0 <- apply(activity,activity$steps=="NAN",activity$steps<-agg[agg$interval==] )
 
 
+act0 <- activity
+
+for (i in 1:17568) {
+    if (is.na(act0[i,1])){
+        agg0<- subset(agg,agg$interval == act0[i,3])
+       act0[i,1] <- agg0$steps
+    }
+}
+
+
+
+
+act1 <- subset(act0,act0$interval==0)
+
+
+# question 4
+
+
+act1 <- activity
+
+
+
+
+library(lubridate)
+
+
+
+library(dplyr)
+
+act3 <- tbl_df(activity)
+
+act4 <- act3 %>% group_by(interval) %>% summarise(mean.steps = mean(steps,na.rm=TRUE))
+
+
+
+
+
+act0$day <- wday(act0$date)
+
+act0$day1 <- ifelse(act0$day %in% 2:6,"weekday","weekend")
+
+act0$day1 <- as.factor(act0$day1)
+
+agg = aggregate(act0,
+                by = list(act0$interval,act0$day1),
+                FUN = mean,
+                na.rm=TRUE)
+
+
+
+plot(act0$steps,act0$day1)
+
+library(lattice)
+with(agg, xyplot(agg$steps ~ agg$Group.1 | agg$Group.2 , type = "l"))
+
+# plot(x,y,ylim=c(0,10),xlim=c(0,5))type = "l"
+
+# wday("2012-10-01")
+
+#1 ocotbre 2012 un lundi
 
 
 
